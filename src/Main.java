535d1029366370fba058207634a93b689e43b2f6
Jannik Lindemeier <jannik@lindemeier-online.de>
2022-04-04 20:40:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> test = new ArrayList<>();
+        System.out.println("");
+    }
+}
